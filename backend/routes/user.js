2a23901444b13cff9c86b2de381d26462fe5c9e2
Jukita <zjukiita@gmail.com>
2021-04-29 19:53:53 -0300

    Sem verify :D

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
 const sha256 = require('js-sha256');
-const verifyJWT = require('../middlewares/auth');
 
 
 router.get('/', /* verifyJWT ,*/  async (req, res) => {
