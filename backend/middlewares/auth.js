db6d976af4b540b0a6af520e39c4dc3258f9d8fd
Julio_Cesar_De_Azevedo <zjukiita@gmail.com>
2021-06-21 14:49:22 -0300

    Sem middlewares

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+/* const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
     const token = req.headers['x-access-token'];
@@ -21,4 +21,4 @@ const verifyJWT = (req, res, next) => {
     });
 }
 
-module.exports = verifyJWT;
+module.exports = verifyJWT; */
